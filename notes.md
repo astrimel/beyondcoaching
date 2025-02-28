535b6aa3b63a4b738b5ac76fb19c4ffa9d8a0efa
astrimel <alexstrimel@gmail.com>
2025-02-28 09:29:19 -0500

    Add files via upload

---
+++ notes.md
@@ -0,0 +1,5 @@
+<!-- TO DO -->
+
+- Play with hero image overlay (maybe alinear gradient?)
+- Center the big text on the WHO section
+- Auto scrolling testimonials
---
+++ script.js
@@ -0,0 +1,38 @@
+//
+// const coaches = [
+//   {
+//     name: "Beth Marshall",
+//     id: 1,
+//     coach: "Coach Beth",
+//     bio: " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//   },
+//   {
+//     name: "Ann Morris",
+//     id: 2,
+//     coach: "Coach Ann",
+//     bio: " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//   },
+// ];
+
+new Swiper(".swiper", {
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+// IMPLEMENTING PAGINATION
+// ✅ When hover over cirlce background color changes
+// When clicked unselected circle, background color on selected circle changes to unselected and vice versa for unselected
+//
